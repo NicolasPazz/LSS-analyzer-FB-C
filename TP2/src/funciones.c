1c357c4bd2892a1c93be558697d748ee23b57e3c
“Nicolas <paz-nico@hotmail.com>
2024-08-08 17:32:51 -0300

    termino punto 4 y algunos arreglos

--- TP2/src/funciones.c
+++ TP2/src/funciones.c
@@ -109,7 +109,7 @@ void imprimirConstantesEnterasDecimales(NodoConstanteEntera *lista) {
     printf("* Listado de constantes enteras decimales:\n");
 
     if (actual == NULL) {
-        printf("- \n");
+        printf("-\n");
         return;
     }
 
@@ -126,7 +126,7 @@ void imprimirConstantesEnterasHexadecimales(NodoConstanteEntera *lista) {
     printf("* Listado de constantes enteras hexadecimales:\n");
 
     if (actual == NULL) {
-        printf("- \n");
+        printf("-\n");
         return;
     }
 
@@ -141,10 +141,10 @@ void imprimirConstantesEnterasOctales(NodoConstanteEntera *lista) {
     printf("* Listado de constantes enteras octales:\n");
 
     if (actual == NULL) {
-        printf("- \n");
+        printf("-\n");
         return;
     }
-    
+
     while (actual != NULL) {
         printf("%s: valor entero decimal %d\n", actual->constanteEntera, (int)strtol(actual->constanteEntera, NULL, 8));
         actual = actual->siguiente;
@@ -153,3 +153,194 @@ void imprimirConstantesEnterasOctales(NodoConstanteEntera *lista) {
 
 //--
 
+NodoConstanteReal* crearNodoConstanteReal(const char *numero) {
+    NodoConstanteReal *nuevoNodoConstanteReal = (NodoConstanteReal *)malloc(sizeof(NodoConstanteReal));
+    nuevoNodoConstanteReal->constanteReal = strdup(numero);
+    
+    nuevoNodoConstanteReal->parteEntera = 0.0;
+    nuevoNodoConstanteReal->mantisa = 0.0;
+    nuevoNodoConstanteReal->siguiente = NULL;
+    
+    return nuevoNodoConstanteReal;
+}
+
+void agregarConstanteReal(NodoConstanteReal **lista, const char *numero) {
+    NodoConstanteReal *nuevo = crearNodoConstanteReal(numero);
+
+    const char *punto = strchr(numero, '.');
+
+    if (punto != NULL) {
+        nuevo->parteEntera = atof(strndup(numero, punto - numero));
+        nuevo->mantisa = atof(punto); // La mantisa comienza en el punto decimal
+    } else {
+        nuevo->parteEntera = atof(numero);
+        nuevo->mantisa = .0;
+    }
+
+    if (*lista == NULL) {
+        *lista = nuevo;
+    } else {
+        NodoConstanteReal *actual = *lista;
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->siguiente = nuevo;
+    }
+}
+
+void imprimirConstantesReales(NodoConstanteReal *lista) {
+    NodoConstanteReal *actual = lista;
+
+    printf("* Listado de constantes reales:\n");
+
+    if (actual == NULL) {
+        printf("-\n");
+        return;
+    }
+
+    while (actual != NULL) {
+        printf("%s: parte entera %.6f, mantisa 0%.6f\n", actual->constanteReal, actual->parteEntera, actual->mantisa);
+        actual = actual->siguiente;
+    }
+}
+
+//--
+
+NodoConstanteCaracter* crearNodoConstanteCaracter(const char *constante) {
+    NodoConstanteCaracter *nuevoNodoConstanteCaracter = (NodoConstanteCaracter *)malloc(sizeof(NodoConstanteCaracter));
+    nuevoNodoConstanteCaracter->constanteCaracter = strdup(constante);
+    nuevoNodoConstanteCaracter->siguiente = NULL;
+    return nuevoNodoConstanteCaracter;
+}
+
+void agregarConstanteCaracter(NodoConstanteCaracter **lista, const char *constante) {
+    NodoConstanteCaracter *nuevo = crearNodoConstanteCaracter(constante);
+    
+    if (*lista == NULL) {
+        *lista = nuevo;
+    } else {
+        NodoConstanteCaracter *actual = *lista;
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->siguiente = nuevo;
+    }
+}
+
+void imprimirConstantesCaracteres(NodoConstanteCaracter *lista) {
+    NodoConstanteCaracter *actual = lista;
+    int indice = 1;
+    
+    printf("* Listado de constantes caracter enumerados:\n");
+    
+    while (actual != NULL) {
+        printf("%d) %s\n", indice++, actual->constanteCaracter);
+        actual = actual->siguiente;
+    }
+}
+
+// Funciones de Operador/CaracterDePuntuacion:
+
+// Función para crear un nuevo nodo de operador/caracter de puntuación
+NodoOperadorYCaracteresDePuntuacion* crearNodoOperadorYCaracteresDePuntuacion(const char *simbolo) {
+    NodoOperadorYCaracteresDePuntuacion*newNode = (NodoOperadorYCaracteresDePuntuacion *)malloc(sizeof(NodoOperadorYCaracteresDePuntuacion));
+    newNode->simbolo = strdup(simbolo);
+    newNode->contador = 1;
+    newNode->siguiente = NULL;
+    return newNode;
+}
+
+
+// Función para agregar un operador/caracter de puntuación en la lista
+void agregarOperadorYCaracteresDePuntuacion(NodoOperadorYCaracteresDePuntuacion **head, const char *simbolo) {
+    NodoOperadorYCaracteresDePuntuacion *actual = *head;
+
+    // Buscar si el operador ya existe
+    while (actual != NULL) {
+        if (strcmp(actual->simbolo, simbolo) == 0) {
+            actual->contador++;
+            return;
+        }
+        actual = actual->siguiente;
+    }
+
+    // Crear un nuevo nodo
+   NodoOperadorYCaracteresDePuntuacion *newNode = crearNodoOperadorYCaracteresDePuntuacion(simbolo);
+
+    // agregar el nuevo nodo al final de la lista
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+       actual= *head;
+        while (actual->siguiente != NULL) {
+            actual= actual->siguiente;
+        }
+        actual->siguiente = newNode;
+    }
+}
+
+// Función para imprimir la lista de operadores/caracteres de puntuación
+void imprimirOperadoresYCaracteresDePuntuacion(NodoOperadorYCaracteresDePuntuacion *head) {
+    NodoOperadorYCaracteresDePuntuacion *actual = head;
+    printf("Listado de operadores/caracteres de puntuación encontrados:\n");
+    while (actual!= NULL) {
+        printf("%s: aparece %d %s\n", actual->simbolo,actual->contador, actual->contador == 1 ? "vez" : "veces");
+        actual = actual->siguiente;
+    }
+}
+
+
+//Funciones de Cadena No Reconocida :
+
+// Función para crear un nuevo nodo de cadena no reconocida
+NodoCadenaNoReconocida* crearNodoCadenaNoReconocida(const char *CadenaNoReconocida, int linea, int columna) {
+    NodoCadenaNoReconocida *newNode = (NodoCadenaNoReconocida *)malloc(sizeof(NodoCadenaNoReconocida));
+    newNode->CadenaNoReconocida = strdup(CadenaNoReconocida);
+    newNode->linea = linea;
+    newNode->columna = columna;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+// Función para agregar una cadena no reconocida en la lista
+void agregarCadenaNoReconocida(NodoCadenaNoReconocida **head, const char *CadenaNoReconocida, int linea, int columna) {
+    NodoCadenaNoReconocida *actual = *head;
+    NodoCadenaNoReconocida *anterior = NULL;
+
+    // Crear un nuevo nodo
+   NodoCadenaNoReconocida *newNode = crearCadenaNoReconocidaNode(CadenaNoReconocida, linea, columna);
+
+    // agregar el nuevo nodo al final de la lista
+    if (actual == NULL) {
+        *head = newNode;
+    } else {
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->next = newNode;
+    }
+}
+// Función para imprimir la lista de cadenas no reconocidas
+void  imprimirCadenasNoReconocidas(NodoCadenaNoReconocida *head) {
+   NodoCadenaNoReconocida *actual = head;
+    printf("Listado de cadenas no reconocidas encontradas:\n");
+    while (actual != NULL) {
+        printf("\"%s\": línea %d, columna %d\n", actual->CadenaNoReconocida, actual->linea, actual->columna);
+        actual = actual->next;
+    }
+}
+
+//--
+//Funcion Para Liberar Memoria
+/*
+void Liberar(lista*head) {
+    lista *actual = head;
+    while (actual!= NULL) {
+       lista *siguiente = actual->siguiente;
+        free(actual->valor);
+        free(actual);
+        actual = siguiente;
+    }
+}
+*/
--- TP2/src/funciones.h
+++ TP2/src/funciones.h
@@ -52,3 +52,51 @@ void imprimirConstantesEnterasOctales(NodoConstanteEntera *lista);
 
 //--
 
+typedef struct NodoConstanteReal {
+    char *constanteReal;             
+    double parteEntera;         
+    double mantisa;         
+    struct NodoConstanteReal *siguiente;
+} NodoConstanteReal;
+
+NodoConstanteReal* crearNodoConstanteReal(const char *numero);
+
+void agregarConstanteReal(NodoConstanteReal **lista, const char *numero);
+
+void imprimirConstantesReales(NodoConstanteReal *lista);
+
+//--
+
+typedef struct NodoConstanteCaracter {
+    char *constanteCaracter;
+    struct NodoConstanteCaracter *siguiente;
+} NodoConstanteCaracter;
+
+NodoConstanteCaracter* crearNodoConstanteCaracter(const char *constante);
+
+void agregarConstanteCaracter(NodoConstanteCaracter **lista, const char *constante);
+
+void imprimirConstantesCaracteres(NodoConstanteCaracter *lista);
+
+// OPERADOR
+typedef struct NodoOperadorYCaracteresDePuntuacion {
+    char *simbolo;
+    int contador;
+    struct NodoOperador *siguiente;
+} NodoOperadorYCaracteresDePuntuacion;
+
+NodoOperadorYCaracteresDePuntuacion* crearNodoOperadorYCaracteresDePuntuacion(const char *simbolo);
+
+void agregarOperadorYCaracteresDePuntuacion(NodoOperador **head, const char *simbolo);
+
+// Cadena No RECONOCIDA
+typedef struct NodoCadenaNoReconocida {
+    char *CadenaNoReconocida;
+    int linea;
+    int columna;
+    struct NodoCadenaNoReconocida *siguiente;
+} NodoCadenaNoReconocida;
+
+NodoCadenaNoReconocida* crearNodoCadenaNoReconocida(const char *CadenaNoReconocida, int linea, int columna);
+
+void agregarCadenaNoReconocida(NodoCadenaNoReconocida **head, const char *CadenaNoReconocida, int linea, int columna);
